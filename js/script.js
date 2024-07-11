c05d58958e3be5178f7b7ac05984d376829381b5
Shivam Bansal <shivambnsal@gmail.com>
2024-07-11 23:29:59 +0530

    Create multiply function and fix add and substract
    Bug : Error if no input is given in function
    Solution : Check if input is empty and return "No Input"

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 function add(...nums) {
+    if(nums.length === 0)   return "No Input";
     return nums.reduce((sum, currentNum) => sum + currentNum, 0);
 }
 
-function substract(firstNum, secondNum) {
-    return firstNum - secondNum;
+function substract(...nums) {
+    if(nums.length === 0)   return "No Input";
+    return nums.reduce((firstNum, secondNum) => firstNum - secondNum);
 }
-console.log(substract(6,4));
+
+function multiply(...nums) {
+    if(nums.length === 0)   return "No Input";
+    return nums.reduce((product, currentNum) => product * currentNum, 1);
+}
+
+console.log(multiply(6,4,2,2));
